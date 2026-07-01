a777514229e1113042dc5a328a9f5a0cab59e096
darkxxx2000 <silvana1002010@gmail.com>
2026-06-30 22:49:33 -0300

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,21 @@
+document
+.getElementById("loginBtn")
+.addEventListener("click",()=>{
 
+    const user=document.getElementById("user").value;
+
+    const pass=document.getElementById("pass").value;
+
+    if(user==="admin" && pass==="1234"){
+
+        window.location="dashboard.html";
+
+    }
+
+    else{
+
+        document.getElementById("msg").innerHTML="Usuario o contraseña incorrectos.";
+
+    }
+
+});
